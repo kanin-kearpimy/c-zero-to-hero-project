5b5d9b7020ecbead87e6e30d2f6395f5cb72e83c
KANIN KEARPIMY <jamesk@KANINs-MacBook-Air.local>
2025-12-26 16:58:17 +0700

    fixed parser error

--- src/parse.c
+++ src/parse.c
@@ -8,7 +8,7 @@
 #include "common.h"
 #include  "parse.h"
 
-int create_db_header(int fd, struct dbheader_t **headerOut) {
+int create_db_header(struct dbheader_t **headerOut) {
     struct dbheader_t *header = (struct dbheader_t *) calloc(1, sizeof(struct dbheader_t));
 
     if(header == NULL) {
